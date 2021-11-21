feecbad6b562e169db349288469dead83e980319
MenterCOX <90411559+MenterCOX@users.noreply.github.com>
2021-11-21 12:57:18 +0300

    Update pom.xml

---
+++ Doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"game","l":"Bonuse"},{"p":"game","l":"FrameController"},{"p":"game","l":"GameLoop"},{"p":"game","l":"KeyController"},{"p":"game","l":"Main"},{"p":"game","l":"Player"},{"p":"game","l":"WallGeneration"},{"p":"game","l":"Writer"}]
