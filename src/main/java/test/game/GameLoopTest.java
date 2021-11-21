030e3e7124fc772e0d17d0a66018ce3d1965c159
MenterCOX <danya.yagnish@mail.ru>
2021-11-21 13:18:37 +0300

    Порядок

---
+++ src/main/java/test/game/GameLoopTest.java
@@ -0,0 +1,26 @@
+package test.game; 
+
+import org.junit.Test; 
+
+/** 
+* GameLoop Tester. 
+* 
+* @author <Cox>
+* @since <pre>но€б. 21, 2021</pre> 
+* @version 1.0 
+*/ 
+public class GameLoopTest { 
+
+
+/** 
+* 
+* Method: loop() 
+* 
+*/ 
+@Test
+public void testLoop() throws Exception { 
+
+} 
+
+
+} 
