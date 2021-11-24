de5c86d630f5b21e3bc81935bfd7c4352e49bbc2
MenterCOX <danya.yagnish@mail.ru>
2021-11-25 00:09:30 +0300

    Страдаю

---
+++ src/test/java/advtile/AdvTileManagerTest.java
@@ -0,0 +1,24 @@
+package advtile;
+
+import junit.framework.Assert;
+import object.AdvSuperObject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdvTileManagerTest {
+
+    @Test
+    void getTileImage() {Assert.assertNotNull(new AdvSuperObject());
+    }
+
+    @Test
+    void loadMap() {Assert.assertNotNull(new AdvSuperObject());
+    }
+
+    @Test
+    void draw() {
+            Assert.assertNotNull(new AdvSuperObject());
+        }
+
+}
